a3e4bb4ec1392e887b6fbb0fd3c7fc6bc236da15
Thong Quoc (Bill) Huynh <huynh@mines.edu>
2024-01-30 00:35:26 -0700

    progress on project maze

--- project-1/Examples/Examples/test.py
+++ project-1/Examples/Examples/test.py
@@ -1,6 +1,10 @@
-""" Reading inputs """
 import sys
+import networkx as nx
+# import matplotlib.pyplot as plt
 
+""" 
+Reading inputs 
+"""
 with open(sys.argv[1], 'r') as my_file:
     input = my_file.readline().strip()
     n,m = map(int, input.split())
@@ -10,7 +14,7 @@ with open(sys.argv[1], 'r') as my_file:
     colors = list(input.split())
 
     input = my_file.readline().strip()
-    captain_start,lucky_start = map(int, input.split())
+    cptn_start,lucky_start = map(int, input.split())
 
     corr_starts = []
     corr_ends = []
@@ -27,7 +31,7 @@ with open(sys.argv[1], 'r') as my_file:
 # print(n)
 # print(m)
 # print(colors)
-# print(captain_start)
+# print(cptn_start)
 # print(lucky_start)
 # print(type(corr_start))
 # print(type(corr_end))
@@ -37,6 +41,48 @@ with open(sys.argv[1], 'r') as my_file:
 # print(corr_ends)
 # print(corr_colors)
 
+
+"""
+Create all nodes (cptn_room,lucky_room)
+"""
+G = nx.DiGraph()
+for i in range(n):
+    for j in range(n):
+        tuple = (i+1,j+1)
+        G.add_node(tuple)
+
+
+""" 
+Create all edges 
+"""
+for k in range(m):
+    for j in range(n-1): # n-1 because last room (goal room) has no color
+        if colors[j] == corr_colors[k]:
+            # add 2 edges on model graph
+            G.add_edge((corr_starts[k],j+1) , (corr_ends[k],j+1))
+            G.add_edge((j+1,corr_starts[k]) , (j+1,corr_ends[k]))
+
+# print(list(G.edges))
+# print("Total # nodes:", G.number_of_nodes())
+# print("Total # edges:", G.number_of_edges())
+
+
+"""
+Plot
+"""
+# val_map = {(1,1):0.0}
+# values = [val_map.get(node, 0.25) for node in G.nodes()]
+# pos = nx.spring_layout(G)
+# nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), 
+#                        node_color = values, node_size = 500)
+# nx.draw_networkx_labels(G, pos)
+# nx.draw_networkx_edges(G, pos, edgelist=list(G.edges), edge_color='b', arrows=True)
+# plt.show()
+
+
+"""
+Run algorithms
+"""
 
 
 
