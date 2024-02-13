5c949834802a219e8871ca6eb71d84a65b2b0e0f
Mabel Reed <mkreed@mines.edu>
2024-02-12 20:44:07 -0700

    Initial idea for the algorithm implemented, will test more tomorrow

--- project-2/code/algorithm.py
+++ project-2/code/algorithm.py
@@ -1,7 +1,27 @@
 import networkx as nx
 
 def algorithm(G):
+    sccs = (G.subgraph(c) for c in nx.strongly_connected_components(G))
     listNodes = []
+    
+    for subgraph in sccs:
+        # create copy so the subgraph can be modified. The original graph actually does not need to be modified. however this might be slow idk
+        c = nx.DiGraph(G.subgraph(subgraph))
+        while not nx.is_directed_acyclic_graph(c):
+            maxDegreeNode = None
+            maxDegree = 0
+            for n in c:
+                if maxDegreeNode is None:
+                    maxDegreeNode = n
+                    maxDegree = c.in_degree(n) + c.out_degree(n)
+                    continue
+                if c.in_degree(n) + c.out_degree(n) > maxDegree:
+                    maxDegreeNode = n
+                    maxDegree = c.in_degree(n) + c.out_degree(n)
+            
+            c.remove_node(maxDegreeNode)
+            listNodes.append(maxDegreeNode)
+
     return listNodes
 
 def create_output(G, filename):
