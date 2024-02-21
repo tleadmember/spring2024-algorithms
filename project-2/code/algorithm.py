793841ebf7e8897955d7e38bb83ea03d73fed1e5
Mabel Reed <mkreed@mines.edu>
2024-02-21 14:15:38 -0700

    copied over old algorithm

--- project-2/code/algorithm.py
+++ project-2/code/algorithm.py
@@ -1,6 +1,30 @@
 import networkx as nx
 from heapdict import heapdict
 
+def algorithm(G):
+    sccs = (G.subgraph(c) for c in nx.strongly_connected_components(G))
+    listNodes = []
+    
+    for subgraph in sccs:
+        # create copy so the subgraph can be modified. The original graph actually does not need to be modified. however this might be slow idk
+        c = nx.DiGraph(G.subgraph(subgraph))
+        while not nx.is_directed_acyclic_graph(c):
+            maxDegreeNode = None
+            maxDegree = 0
+            for n in c:
+                if maxDegreeNode is None:
+                    maxDegreeNode = n
+                    maxDegree = c.in_degree(n) + c.out_degree(n)
+                    continue
+                if c.in_degree(n) + c.out_degree(n) > maxDegree:
+                    maxDegreeNode = n
+                    maxDegree = c.in_degree(n) + c.out_degree(n)
+            
+            c.remove_node(maxDegreeNode)
+            listNodes.append(maxDegreeNode)
+
+    return listNodes
+
 def algorithmPriorityQueue(G):
     sccs = (G.subgraph(c) for c in nx.strongly_connected_components(G))
     listNodes = []
