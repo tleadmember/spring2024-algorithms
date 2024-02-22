ea3a3f4518e1ec5213938c23dc5025fe237d00a1
Thong Quoc (Bill) Huynh <huynh@mines.edu>
2024-02-21 19:39:30 -0700

    add check for complete graph (faster), but still delete all most common nodes

--- project-2/code/algorithm.py
+++ project-2/code/algorithm.py
@@ -1,16 +1,26 @@
 import networkx as nx
 from collections import defaultdict
 
+def is_complete(G):
+    nodeList = list(G)
+    H = G.subgraph(nodeList)
+    n = len(nodeList)
+    return H.size() == n*(n-1)
+
+
 def algorithm(G):
-    sccs = nx.strongly_connected_components(G)
     listNodes = []
     
-    for scc in sccs:
+    # check if a complete graph
+    if is_complete(G):
+        print("Complete graph detected!")
+        listNodes = list(G)[1:] # delete everything but one node
+    else:
+        H = G.subgraph(list(G)) 
+        H = H.copy()    # create subgraph copy of G to modify
         # create copy so the subgraph can be modified. The original graph actually does not need to be modified. however this might be slow idk
-        sg = G.subgraph(scc)
-        c = sg.copy()
-        while not nx.is_directed_acyclic_graph(c):
-            cycles = nx.simple_cycles(c, length_bound=c.number_of_nodes())
+        while not nx.is_directed_acyclic_graph(H):
+            cycles = nx.simple_cycles(H, length_bound=H.number_of_nodes())
             cyclesLst = [*cycles]
             # print("Cycles list: " + str(cyclesLst))
             # Find most common node(s) in cyclesLst (does not have to be in ALL cycles)
@@ -23,7 +33,7 @@ def algorithm(G):
             # print("Common node(s):" + str(commonNodes))
             # Remove all common nodes
             for commonNode in commonNodes:
-                c.remove_node(commonNode)
+                H.remove_node(commonNode)
                 listNodes.append(commonNode)
 
 
---
+++ project-2/code/testing.py
@@ -0,0 +1,4 @@
+import networkx as nx
+
+G = nx.path_graph(4)
+print(list(G)[1:])
