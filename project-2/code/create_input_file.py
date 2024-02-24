4d3a45785ef24a3dee31fb04750ccb0bd0fedcb4
Bill Huynh <billhuynh-dyalab@TQBH-dyalab.dyalab>
2024-02-23 22:06:50 -0700

    temporary progress on using find_cycle() instead of simple_cycles(), faster

--- project-2/code/create_input_file.py
+++ project-2/code/create_input_file.py
@@ -10,9 +10,9 @@ def convert(lst):
 
 if __name__ == "__main__":
     # User inputs
-    num_nodes = 316 # bound is 10^4 nodes, 10^5 edges
+    num_nodes = 17 # bound is 10^4 nodes, 10^5 edges
     debug = False
-    filename = "inputs/complete316.txt"
+    filename = "inputs/complete17.txt"
 
     # Use networkx to design your input graph (CAN BE MODIFIED)
     G = nx.complete_graph(range(1,num_nodes+1), create_using=nx.DiGraph)
