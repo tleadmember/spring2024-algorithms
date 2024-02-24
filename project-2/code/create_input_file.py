01493f019c0741182369323c639a176492609c88
Mabel Reed <mkreed@mines.edu>
2024-02-23 17:21:20 -0700

    Revert "moved writing graph to a file into a function and added a check for exceeding the number of nodes or edges"
    This reverts commit 0992058f1dcaf7bb016281b9258f713701a315b2.

--- project-2/code/create_input_file.py
+++ project-2/code/create_input_file.py
@@ -1,6 +1,5 @@
 import networkx as nx
 from functools import reduce
-from colorama import Fore
  
 def convert(lst):
     # using reduce function to cumulatively apply lambda function to each element
@@ -8,18 +7,32 @@ def convert(lst):
     lst_string = map(str, lst)
     return reduce(lambda x,y: x + ' ' + y, lst_string)
 
-def write_graph_to_file(G):
+
+if __name__ == "__main__":
+    # User inputs
+    num_nodes = 316 # bound is 10^4 nodes, 10^5 edges
+    debug = False
+    filename = "inputs/complete316.txt"
+
+    # Use networkx to design your input graph (CAN BE MODIFIED)
+    G = nx.complete_graph(range(1,num_nodes+1), create_using=nx.DiGraph)
+
+    # Print out edges for debug
+    if debug:
+        for edge in G.edges:
+            start_node = str(edge[0]).replace(', ', "").replace(")", "").replace("(", "")
+            end_node = str(edge[1]).replace(', ', "").replace(")", "").replace("(", "")
+            print(str(start_node) + " -> " + str(end_node))
+    
+    # # Convert from NetworkX graph to Graphviz graph (linux only, apparently)
+    # A = nx.nx_agraph.to_agraph(G)
+    # # Set graph layout style and draw
+    # A.layout('dot')
+    # A.draw('networkx_graph2.png')
+
+
     # Translate networkx graph into input file format
-    maxNumNodes = 10000
-    maxNumEdges = 100000
-
-    if G.number_of_nodes() > maxNumNodes:
-        print(Fore.RED + "WARNING, SADNESS: GENERATED INPUT EXCEEDED THE MAXIMUM NUMBER OF NODES WITH [" + G.number_of_nodes() + "] NODES")
-        
-    if G.number_of_edges() > maxNumEdges:
-        print(Fore.RED + "WARNING, SADNESS: GENERATED INPUT EXCEEDED THE MAXIMUM NUMBER OF EDGES WITH [" + G.number_of_nodes() + "] EDGES")
-
-    listsPrereqs = [[] for _ in range(G.number_of_nodes())]
+    listsPrereqs = [[] for _ in range(num_nodes)]
 
     for edge in G.edges:
         end_node = edge[1]
@@ -39,30 +52,3 @@ def write_graph_to_file(G):
                 file.write(convert(listsPrereqs[i]))
             if i < total_length-1:
                 file.write("\n")
-
-def generate_graph(num_nodes):
-    return nx.complete_graph(range(1,num_nodes+1), create_using=nx.DiGraph)
-
-if __name__ == "__main__":
-    # User inputs
-    num_nodes = 316 # bound is 10^4 nodes, 10^5 edges
-    debug = False
-    filename = "inputs/finalinput.txt"
-
-    # Use networkx to design your input graph (CAN BE MODIFIED)
-    G = generate_graph(num_nodes)
-
-    # Print out edges for debug
-    if debug:
-        for edge in G.edges:
-            start_node = str(edge[0]).replace(', ', "").replace(")", "").replace("(", "")
-            end_node = str(edge[1]).replace(', ', "").replace(")", "").replace("(", "")
-            print(str(start_node) + " -> " + str(end_node))
-    
-    # # Convert from NetworkX graph to Graphviz graph (linux only, apparently)
-    # A = nx.nx_agraph.to_agraph(G)
-    # # Set graph layout style and draw
-    # A.layout('dot')
-    # A.draw('networkx_graph2.png')
-            
-    write_graph_to_file(G)
