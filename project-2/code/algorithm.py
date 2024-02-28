ea242360062a23e926cbabfbac05e452120b65d6
Mabel Reed <mkreed@mines.edu>
2024-02-28 15:22:17 -0700

    should fix algorithm.py from main

--- project-2/code/algorithm.py
+++ project-2/code/algorithm.py
@@ -58,13 +58,13 @@ def algorithm(G):
         startTime = timer()
         count = 0
         # Do algorithm random for maxTime seconds or 1000 times (will we win the lottery???)
-        while timer() - startTime < sccMaxTime and count < 1000:
-            sys.stdout.flush()
-            c = nx.DiGraph(G.subgraph(subgraph))
-            tmp = []
-            algorithmRandom(c, tmp)
-            listListNodes.append(tmp)
-            count += count
+        # while timer() - startTime < sccMaxTime and count < 1000:
+        #     sys.stdout.flush()
+        #     c = nx.DiGraph(G.subgraph(subgraph))
+        #     tmp = []
+        #     algorithmRandom(c, tmp)
+        #     listListNodes.append(tmp)
+        #     count += count
 
         print("done with initial random")
         sys.stdout.flush()
@@ -129,8 +129,8 @@ def algorithm(G):
         startTime = timer()
         print("starting random with starting point")
         sys.stdout.flush()
-        huh = algorithmRandomWithStartingPoint(c, tmp, minimumListNodes, startTime, sccMaxTime)
-        listNodes.extend(huh)
+        algorithmRandomWithStartingPoint(c, tmp, minimumListNodes, startTime, sccMaxTime)
+        listNodes.extend(tmp)
         print("done with random with starting point")
         sys.stdout.flush()
 
