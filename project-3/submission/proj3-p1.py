12c0250101d1a7673c07ad16b690ea4b678ef981
Thong Quoc (Bill) Huynh <huynh@mines.edu>
2024-03-26 23:55:05 -0600

    [feature] project 3, part 1, first attempt

---
+++ project-3/submission/proj3-p1.py
@@ -0,0 +1,42 @@
+import sys
+
+""" 
+Reading inputs 
+"""
+with open(sys.argv[1], 'r') as my_file:
+    input = my_file.readline().strip()
+    n = int(input)
+    input = my_file.readline().strip()
+    segments_list = list(map(int, input.split()))
+    
+"""
+Prefix sum
+"""
+pf_sum_list = [0]*n
+pf_sum_list[0] = segments_list[0]
+for i in range(1, n):
+    pf_sum_list[i] = pf_sum_list[i-1] + segments_list[i]
+
+# print(n)
+# print(segments_list)
+# print(segments_list[0])
+# print(n+segments_list[0])
+# print(pf_sum_list)
+
+"""
+Naive recursion
+"""
+def T(i,j):
+    if i == 0:
+        length = pf_sum_list[j]
+    else:
+        length = pf_sum_list[j] - pf_sum_list[i-1]
+    if i == j: # base case
+        return length
+    return length - min(T(i+1,j), T(i,j-1))
+
+
+"""
+Output
+"""
+print( T(0, n-1) )
