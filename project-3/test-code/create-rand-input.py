d3803fa60ec1069dc45cf7e805a57b0a7e97241b
BillH-pcs <huynh@mines.edu>
2024-03-29 19:01:47 -0600

    [feature] completed Part 1 of Project DP

---
+++ project-3/test-code/create-rand-input.py
@@ -0,0 +1,15 @@
+import random
+
+"""
+Generate random input files
+"""
+for n in range(2, 21, 2):
+    x = n//2
+    file_name = "sample--" + str(x) + ".txt"
+    with open(file_name, "w") as file:
+        file.write(str(n))
+        file.write("\n")
+        for _ in range(n):
+            rand_num = random.randrange(1,1000)
+            file.write(str(rand_num))
+            file.write(" ")
--- project-3/test-code/proj3-p1.py
+++ project-3/test-code/proj3-p1.py
@@ -1,17 +1,17 @@
 import sys
+from timeit import default_timer as timer
 
-""" 
-Reading inputs 
-"""
+""" Reading inputs """
 with open(sys.argv[1], 'r') as my_file:
     input = my_file.readline().strip()
     n = int(input)
     input = my_file.readline().strip()
     segments_list = list(map(int, input.split()))
     
-"""
-Prefix sum
-"""
+""" Start timing """
+start = timer()
+
+""" Prefix sum """
 pf_sum_list = [0]*n
 pf_sum_list[0] = segments_list[0]
 for i in range(1, n):
@@ -23,9 +23,7 @@ for i in range(1, n):
 # print(n+segments_list[0])
 # print(pf_sum_list)
 
-"""
-Naive recursion
-"""
+""" Naive recursion """
 def T(i,j):
     if i == 0:
         length = pf_sum_list[j]
@@ -35,8 +33,11 @@ def T(i,j):
         return length
     return length - min(T(i+1,j), T(i,j-1))
 
-
-"""
-Output
-"""
+""" Output """
 print( T(0, n-1) )
+
+""" End timing """
+end = timer()
+msg = "Time elapsed: {} ms."
+formatted_msg = msg.format((end-start)*1000)
+print(formatted_msg)
